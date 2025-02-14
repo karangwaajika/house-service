355b0bc3ebdbec611f821adbc84dd8f3d61c8b6c
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-02-14 09:40:08 +0200

    fix: reload windows log in with google

--- client/src/utils/GoogleRedirectHandler.jsx
+++ client/src/utils/GoogleRedirectHandler.jsx
@@ -1,38 +1,44 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import { GOOGLE_ACCESS_TOKEN } from './token'
+import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { GOOGLE_ACCESS_TOKEN } from "./token";
 
 function GoogleRedirectHandler() {
-    const navigate = useNavigate()
-    useEffect(()=>{
-        console.log("Redirect handler")
-        const queryParams = new URLSearchParams(window.location.search)
-        const accessToken = queryParams.get('access_token')
-        console.log("queryparams", window.location.search)
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log("Redirect handler");
+    const queryParams = new URLSearchParams(window.location.search);
+    const accessToken = queryParams.get("access_token");
+    console.log("queryparams", window.location.search);
 
-        if(accessToken){
-            console.log("A.Token found", accessToken)
-            localStorage.setItem(GOOGLE_ACCESS_TOKEN, accessToken)
+    if (accessToken) {
+      console.log("A.Token found", accessToken);
+      localStorage.setItem(GOOGLE_ACCESS_TOKEN, accessToken);
 
-            // verify token from backend
-            axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
-            axios.get('http://127.0.0.1:8000/api/auth/user/').then(response =>{
-                console.log('User data', response.data)
-                navigate('/dashboard/home')
-            }).catch(error =>{
-                console.error("error verify token", error.response? error.response.data: error.message)
-                navigate('/login')
-            })
-        }else{
-            console.log("no token found")
-            navigate('/login')
-        }
-    }, [navigate])
-    
-    return <div>Logging in .....</div>
- 
+      // verify token from backend
+      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+      axios
+        .get("http://127.0.0.1:8000/api/auth/user/")
+        .then((response) => {
+          console.log("User data", response.data);
+          navigate("/dashboard/home");
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error(
+            "error verify token",
+            error.response ? error.response.data : error.message
+          );
+          navigate("/login");
+        });
+    } else {
+      console.log("no token found");
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  return <div>Logging in .....</div>;
 }
 
-export default GoogleRedirectHandler
+export default GoogleRedirectHandler;
