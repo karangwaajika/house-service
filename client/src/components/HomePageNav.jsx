817c2b71bc0b95e479976c46ad8443e2b86871a4
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-02-15 18:09:32 +0200

    style: remove logo on navbar

--- client/src/components/HomePageNav.jsx
+++ client/src/components/HomePageNav.jsx
@@ -1,29 +1,28 @@
-import logo from "/images/cock.avif";
+import logo from "/images/logo.png";
 
 export default function HomePageNav() {
   return (
     <nav className="navbar-home">
       <div className="left-info">
-        <div className="logo">
+        <div className="logo--home">
           <img src={logo} alt="resto-logo" width={100} height={100} />
         </div>
-        
+
         <ul className="links">
           <li>
-            <a href="/service/home">Home</a>
+            <a href="/">Home</a>
           </li>
           <li>
-            <a href="/service/orders">Services</a>
+            <a href="/">Services</a>
           </li>
           <li>
-            <a href="/service/service">About</a>
+            <a href="/">About</a>
           </li>
           <li>
-            <a href="/service/my-services">Contacts</a>
+            <a href="/">Contacts</a>
           </li>
         </ul>
       </div>
-      
     </nav>
   );
 }
