1ddfd01d4bcee617a231bb128315c1692341fcfa
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-02-15 14:56:23 +0200

    feat: display categories on homepage

--- client/src/components/ServiceCategory.jsx
+++ client/src/components/ServiceCategory.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import picture from "/images/google.png";
 import useFetchAll from "../hooks/useFetchAll";
 
 function ServiceCategory() {
   const { data } = useFetchAll("/api/categories/no_pagination/");
+  const [isClicked, setIsClicked] = useState({ id: null, status: false });
+  const handleClicked = (id) => {
+    setIsClicked({ id: id, status: true });
+  };
   return (
     <div className="service-category">
       <div className="category-header">
@@ -13,13 +17,34 @@ function ServiceCategory() {
       </div>
       <div className="category-section">
         <div className="category-list">
-          {data.length > 0 && data.map((item, i) => {
-            return (
-              <div className="grid">
-                <img src={item.images[0]["image"]} alt="good" width={100} height={100} />
-              </div>
-            );
-          })}
+          {data.length > 0 &&
+            data.map((item, i) => {
+              // let style = {
+              //   backgroundImage: `url(${item.images[0].image})`,
+              //   backgroundSize: "contain",
+              //   backgroundRepeat: "no-repeat",
+              //   backgroundPosition: "top",
+              // };
+              return (
+                <div
+                  className={
+                    isClicked.id == item.id && isClicked.status
+                      ? "grid active"
+                      : "grid"
+                  }
+                  onClick={() => handleClicked(item.id)}
+                  key={i}
+                >
+                  <img
+                    src={item.images[0].image}
+                    alt={`photo${i}`}
+                    width={100}
+                    height={100}
+                  />
+                  <span className="cate--text">{item.name}</span>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
