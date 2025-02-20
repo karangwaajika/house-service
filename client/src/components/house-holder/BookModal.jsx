dd2db6751a4a4ef5192ad61e72a31bd61c12e2ef
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-02-20 10:37:47 +0200

    feat: add time slot for booking

--- client/src/components/house-holder/BookModal.jsx
+++ client/src/components/house-holder/BookModal.jsx
@@ -4,14 +4,36 @@ import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import Button from "../ui/Button";
 
 function BookModal({ animate, handleCloseModal }) {
   const [value, setValue] = useState(dayjs(Date.now()));
   console.log(value.format("YYYY-MM-DD"));
   const closeModal = (e) => {
-    if (e.target.className == `modal ${animate}`) {
+    if (e.target.className == `modal-house ${animate}`) {
       handleCloseModal();
     }
+  };
+  const timeSlot = [
+    "10:00 AM",
+    "10:30 AM",
+    "11:00 AM",
+    "11:30 AM",
+    "12:00 AM",
+    "12:30 PM",
+    "01:00 PM",
+    "01:30 PM",
+    "02:00 PM",
+    "02:30 PM",
+    "03:00 PM",
+    "03:30 PM",
+    "04:00 PM",
+    "04:30 PM",
+    "05:00 PM",
+  ];
+  const [clicked, setClicked] = useState({ status: false, id: null });
+  const handleClicked = (index) => {
+    setClicked({ status: true, id: index });
   };
   return (
     <div className={`modal-house ${animate}`} onClick={closeModal}>
@@ -27,7 +49,7 @@ function BookModal({ animate, handleCloseModal }) {
 
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DemoContainer components={["DateCalendar", "DateCalendar"]}>
-            <DemoItem >
+            <DemoItem>
               <DateCalendar
                 value={value}
                 onChange={(newValue) => setValue(newValue)}
@@ -36,6 +58,42 @@ function BookModal({ animate, handleCloseModal }) {
             </DemoItem>
           </DemoContainer>
         </LocalizationProvider>
+        <h5>Select Time</h5>
+        <div className="time--container">
+          {timeSlot.map((item, i) => {
+            let className = "time--slot";
+            if (item == "03:00 PM") {
+              className = "time--slot active";
+            }
+            if (clicked.status && i == clicked.id) {
+              className = "time--slot clicked";
+            }
+            return (
+              <div
+                className={className}
+                key={i}
+                onClick={() => handleClicked(i)}
+              >
+                <span>{item}</span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="buttons">
+          <Button
+            className="btn-danger"
+            text="Cancel"
+            height={30}
+            width={100}
+            onClick={handleCloseModal}
+          />
+          <Button
+            className="btn-primary-book"
+            text="Book"
+            height={30}
+            width={100}
+          />
+        </div>
       </div>
     </div>
   );
